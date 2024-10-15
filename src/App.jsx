ec4157d0b7a4f910a34b7178a64a5d6ab5ff8bfa
antoniocarlosrs <antoniocarlosrs.dev@gmail.com>
2024-10-14 22:21:13 -0300

    Inicialização Projeto

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { Title, Container, Input, ContainerInputs, Button, InputLabel, TopBackground, Form } from "./styles"
+
+function Home() {
+  return (
+    <Container>
+      <TopBackground>
+        <img src="" alt="" />
+      </TopBackground>
+
+      <Form>
+
+        <Title>Cadastrar Usuários</Title>
+
+        <ContainerInputs>
+          <div>
+            <div>
+              <InputLabel>
+                Nome<span>*</span>
+              </InputLabel>
+              <Input type="text" placeholder="Nome do Usuário"/>
+            </div>
+
+            <div>
+            <InputLabel>
+                Idade<span>*</span>
+              </InputLabel>
+              <Input type="number" placeholder="Idade do Usuário"/>
+            </div>
+          </div>
+
+            <div>
+            <InputLabel>
+                E-mail<span>*</span>
+              </InputLabel>
+              <Input type="email" placeholder="E-mail do usuário"/>
+            </div>
+        </ContainerInputs>
+
+        <Button>Cadastrar Usuário</Button>
+
+      </Form>
+
+    </Container>
+  )
+}
+export default Home
+
