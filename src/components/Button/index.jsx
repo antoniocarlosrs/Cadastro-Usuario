684ccd0eed9875ed6e879fcc6a77e95e9d4477ab
antoniocarlosrs <antoniocarlosrs.dev@gmail.com>
2024-10-21 22:11:41 -0300

    Validações PropTypes

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 import { Button } from './styles'
 
 function DefaulButton({children, ...props}){
@@ -5,6 +6,11 @@ function DefaulButton({children, ...props}){
     return (
         <Button {...props}>{children}</Button>
     )
+}
+
+// Validações TropTypes
+DefaulButton.propTypes = {
+    children: PropTypes.node.isRequired
 }
 
 export default DefaulButton
