f55219d50f0fd5bb6601d3839147aab1f69a265a
antoniocarlosrs <antoniocarlosrs.dev@gmail.com>
2024-10-23 22:24:43 -0300

    Acesso da API useEffect, e colocar array na tela map, useState

--- src/pages/ListUsers/index.jsx
+++ src/pages/ListUsers/index.jsx
@@ -1,16 +1,38 @@
 /*Pagina de Listagem de Usuário */
-import Button from '../../components/Button'
-import TopBackground from '../../components/TopBackground'
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+import Button from "../../components/Button";
+import TopBackground from "../../components/TopBackground";
 
-function ListUsers(){
+function ListUsers() {
+    const [users, setUsers] = useState([])
 
-    return(
+    // Buscar os usuarios na API
+    useEffect(() => {
+        async function getUsers() {
+        const { data } = await api.get("/usuarios");
+        setUsers(data)
+        }
+        getUsers();
+    }, []);
+
+    return (
         <div>
-            <TopBackground></TopBackground>
-            <h1>Listagem de Usuários</h1>
-            <Button>Voltar</Button>
+        <TopBackground></TopBackground>
+        <h1>Listagem de Usuários</h1>
+
+        {/*Estrutura p/ Colocar array na tela*/}
+        {users.map(user => (
+            <div key={user.id}> {/* Chave de indent. id */}
+                <p>{user.name}</p>
+                <p>{user.age}</p>
+                <p>{user.email}</p>
+            </div>
+        ))}
+
+        <Button>Voltar</Button>
         </div>
-    )
+    );
 }
 
-export default ListUsers
+export default ListUsers;
