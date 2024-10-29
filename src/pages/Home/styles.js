e8b0d8c91c90d460afa748a0bac9599c8ff8b94f
antoniocarlosrs <antoniocarlosrs.dev@gmail.com>
2024-10-28 21:33:36 -0300

    Alteração background

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,7 +2,8 @@
 import styled from "styled-components"
 
 export const Container = styled.div`
-    background-color: #181f36;
+    /* background-color: #181f36; */
+    background-color: #25202E;
     display: flex;
     flex-direction: column;
     align-items: center;
